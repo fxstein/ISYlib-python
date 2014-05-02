e64d93ee21050015e2af3398535073d7a18e49cf
Oliver Ratzesberger <oliver@ratzesberger.com>
2014-05-01 22:16:29 -0700

    added config file and keyring support for isy_showevents.py

--- bin/isy_showevents.py
+++ bin/isy_showevents.py
@@ -3,20 +3,28 @@ __author__ = "Peter Shipley"
 
 
 import os
+import keyring
+import ConfigParser
 
 from  ISY.IsyEvent import ISYEvent
 
 def main() :
+
+    config = ConfigParser.ConfigParser()
+    config.read('isy.cfg')
+    
     server = ISYEvent()
 
     # you can subscribe to multiple devices
     # server.subscribe('10.1.1.25')
 
+    isy_addr = config.get('isy', 'addr')
+    isy_user = config.get('isy', 'user')
+
     server.subscribe(
-	addr=os.getenv('ISY_ADDR', '10.1.1.36'),
-	userl=os.getenv('ISY_USER', "admin"),
-	userp=os.getenv('ISY_PASS', "admin")
-    )
+	addr=isy_addr,
+	userl=isy_user,
+	userp=keyring.get_password("isy", isy_user) )
 
     server.set_process_func(ISYEvent.print_event, "")
 
