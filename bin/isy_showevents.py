f994ab4a70a55706eeff133dd484dfd80a68e108
Oliver Ratzesberger <oliver@ratzesberger.com>
2014-06-25 16:26:18 -0700

    Remove keyring and change to basic cfg file handling

--- bin/isy_showevents.py
+++ bin/isy_showevents.py
@@ -3,7 +3,6 @@ __author__ = "Peter Shipley"
 
 
 import os
-import keyring
 import ConfigParser
 
 from  ISY.IsyEvent import ISYEvent
@@ -11,28 +10,24 @@ from  ISY.IsyEvent import ISYEvent
 def main() :
 
     config = ConfigParser.ConfigParser()
-    config.read(os.path.expanduser('~/isy.cfg'))
+    config.read(os.path.expanduser('~/home.cfg'))
     
     server = ISYEvent()
 
-    # you can subscribe to multiple devices
-    # server.subscribe('10.1.1.25')
-
     isy_addr = config.get('isy', 'addr')
     isy_user = config.get('isy', 'user')
+    isy_pass = config.get('isy', 'pass')
 
     server.subscribe(
 	addr=isy_addr,
 	userl=isy_user,
-	userp=keyring.get_password("isy", isy_user) )
+	userp=isy_pass )
 
     server.set_process_func(ISYEvent.print_event, "")
 
     try:
 	print('Use Control-C to exit')
 	server.events_loop()   #no return
-    #    for d in  server.event_iter( ignorelist=["_0", "_11"] ):
-    #	server.print_event(d, "")
     except KeyboardInterrupt:
 	print('Exiting')
 
